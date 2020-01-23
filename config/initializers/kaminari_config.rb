c2a7470d6454c24af68659fb19ae7c7e840ce5d4
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-01-23 10:46:59 +0900

    fix pagenation

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 Kaminari.configure do |config|
-  config.default_per_page = 999 #1ページに表示される数
+  config.default_per_page = 10 #1ページに表示される数
   # config.max_per_page = nil #1ページの最大表示数
-  config.window = 3            #現在のページの左右に表示する数
+  config.window = 10            #現在のページの左右に表示する数
   # config.outer_window = 1
   # config.left = 0
   # config.right = 0
