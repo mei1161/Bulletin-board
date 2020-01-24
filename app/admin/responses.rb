ad660fb2542073dc132f3f113031fb1865ce8b80
mei1161 <ichigo_happy@ezweb.ne.jp>
2020-01-24 12:18:36 +0900

    fix active_admin

--- app/admin/responses.rb
+++ app/admin/responses.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Board do
-  actions :all, except: [:new]
+ActiveAdmin.register Response do
+  actions :all, except: [:new, :update]
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-   permit_params :name
+   permit_params :name, :reply_id, :user_id, :email, :content, :ipaddress, :board_id, :deleted_at
   #
   # or
   #
   # permit_params do
-  #   permitted = [:name, :deleted_at]
+  #   permitted = [:name, :reply_id, :user_id, :email, :content, :ipaddress, :board_id, :deleted_at]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
@@ -19,6 +19,7 @@ ActiveAdmin.register Board do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :content
     end
     f.actions
   end
@@ -28,6 +29,7 @@ ActiveAdmin.register Board do
     id_column
     column :name
     column :created_at
+    column :content
     actions
   end
   
